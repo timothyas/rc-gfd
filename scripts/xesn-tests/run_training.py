ac78d57946f8df71177942b31b5baf58c637d6d3
timothyas <smith.timothy.andrew@gmail.com>
2023-10-30 22:36:11 +0000

    decently comprehensive slots check

--- scripts/xesn-tests/run_training.py
+++ scripts/xesn-tests/run_training.py
@@ -1,7 +1,7 @@
 
 import os
 from dask_jobqueue import SLURMCluster
-from distributed import Client, performance_report
+from distributed import Client, performance_report, LocalCluster
 from xesn import Driver
 
 from distributed.diagnostics import MemorySampler
@@ -22,43 +22,62 @@ def ms_to_xarray(ms, name):
     xda = xr.DataArray(vals, coords={'delta_t':delta_t}, dims=('delta_t',), name=name)
     return xda.to_dataset()
 
-def main(branch):
 
-    workers_per_node = 8
-    n_nodes = 2
+def get_client(n_nodes, output_directory, workers_per_node=8):
 
+    if n_nodes == 1:
+        cluster = LocalCluster(
+            local_directory=os.path.join(output_directory, "worker-logs"),
+            n_workers=workers_per_node,
+        )
+    else:
+        cluster = SLURMCluster(
+            log_directory=os.path.join(output_directory, "worker-logs"),
+            processes=workers_per_node,
+            queue=os.getenv("SLURM_JOB_PARTITION"),
+            job_script_prologue=[
+                "source /contrib/Tim.Smith/miniconda3/etc/profile.d/conda.sh",
+                "conda activate xesn",
+            ],
+        )
+        cluster.scale(jobs=n_nodes)
+
+    client = Client(cluster)
+
+    if n_nodes > 1:
+        client.wait_for_workers(n_nodes*workers_per_node)
+    return client
+
+
+def main(branch, n_nodes, n_years):
+
+    outname = f"output-training-{branch}-{n_years}year-{n_nodes}node"
+    experiment = outname[7:].replace("-", "_")
     driver = Driver(
         config="config-sqg.yaml",
-        output_directory=f"output-training-{branch}",
+        output_directory=outname,
     )
-    cluster = SLURMCluster(
-        log_directory=os.path.join(driver.output_directory, "worker-logs"),
-        processes=workers_per_node,
-        queue=os.getenv("SLURM_JOB_PARTITION"),
-        job_script_prologue=[
-            "source /contrib/Tim.Smith/miniconda3/etc/profile.d/conda.sh",
-            "conda activate xesn",
-        ],
-    )
-    cluster.scale(jobs=n_nodes)
-    client = Client(cluster)
+    driver.overwrite_params({"xdata": {"subsampling": {"time": {"training": [None, n_years*103_680, None]}}}})
+
+    client = get_client(n_nodes=n_nodes, output_directory=driver.output_directory)
 
     ms = MemorySampler()
     report = os.path.join(driver.output_directory, "dask-report.html")
-    with performance_report(report), ms.sample(branch):
+    with performance_report(report), ms.sample(experiment):
         driver.run_training()
     client.close()
 
     ms.plot(align=True)
-    plt.savefig(f"{driver.output_directory}/memory_sampling.pdf", bbox_to_inches="tight")
+    plt.savefig(f"{driver.output_directory}/memory_sampling.pdf", bbox_inches="tight")
 
-    mds = ms_to_xarray(ms, name=branch)
+    mds = ms_to_xarray(ms, name=experiment)
     mds.to_netcdf(f"{driver.output_directory}/memory_sampling.nc")
 
 
 if __name__ == "__main__":
     params = {
-        "branch": "main",
+        "branch": "slots",
+        "n_nodes": 1,
+        "n_years": 1,
     }
     submit_slurm_job("run_training", "main", params, partition="compute")
-

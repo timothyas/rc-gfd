f9a3e441223b2b83c18c336f6254a78169f22384
timothyas <smith.timothy.andrew@gmail.com>
2023-10-30 20:46:56 +0000

    create client; general debug

--- scripts/xesn-tests/run_training.py
+++ scripts/xesn-tests/run_training.py
@@ -1,12 +1,26 @@
 
 import os
 from dask_jobqueue import SLURMCluster
-from distributed import Client
+from distributed import Client, performance_report
 from xesn import Driver
+
+from distributed.diagnostics import MemorySampler
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
 
 import sys
 sys.path.insert(0, "../..")
 from pw import submit_slurm_job
+
+def ms_to_xarray(ms, name):
+
+    table = ms.to_pandas()
+    time = [np.datetime64(x) for x in table.index]
+    delta_t = time - time[0]
+    vals = [x for x in table[name].values]
+    xda = xr.DataArray(vals, coords={'delta_t':delta_t}, dims=('delta_t',), name=name)
+    return xda.to_dataset()
 
 def main(branch):
 
@@ -21,11 +35,26 @@ def main(branch):
         log_directory=os.path.join(driver.output_directory, "worker-logs"),
         processes=workers_per_node,
         queue=os.getenv("SLURM_JOB_PARTITION"),
+        job_script_prologue=[
+            "source /contrib/Tim.Smith/miniconda3/etc/profile.d/conda.sh",
+            "conda activate xesn",
+        ],
     )
     cluster.scale(jobs=n_nodes)
+    client = Client(cluster)
 
-    driver.run_training()
+    ms = MemorySampler()
+    report = os.path.join(driver.output_directory, "dask-report.html")
+    with performance_report(report), ms.sample(branch):
+        driver.run_training()
     client.close()
+
+    ms.plot(align=True)
+    plt.savefig(f"{driver.output_directory}/memory_sampling.pdf", bbox_to_inches="tight")
+
+    mds = ms_to_xarray(ms, name=branch)
+    mds.to_netcdf(f"{driver.output_directory}/memory_sampling.nc")
+
 
 if __name__ == "__main__":
     params = {
